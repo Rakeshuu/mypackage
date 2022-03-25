e5d89f996da9ca13857777b5365ca0a653d216ef
Lenovo <Lenovo@Bibhudatta>
2022-03-25 11:03:48 +0530

    java docs added

--- src/mypkg/CreateJavs.java
+++ src/mypkg/CreateJavs.java
@@ -1,13 +1,21 @@
 package mypkg;
-
+/**
+ * 
+ * 
+ * 
+ */
 public class CreateJavs {
-
+/**
+ * 
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("I have made the changes");
 		System.out.println("update");
         System.out.println("bibhu is uploading to separate branch");
-        System.out.println("after everyhting done");
+        System.out.println("Java docs added");
      
         
         
